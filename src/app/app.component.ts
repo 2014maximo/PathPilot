c5c47e58dc196cbccfb31d566c3298ebe9674424
auriel1 <auriel@smartsoftlabs.com>
2025-05-26 00:37:45 -0500

    testing initials screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PathPilot';
+  jsonData: any;
+
+  onFileChange(evt: any) {
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) return;
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      this.jsonData = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
 }
